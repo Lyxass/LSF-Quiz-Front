5b357c7b28e6bfec36fd27aaaf9348d115ed52ef
Lyxas <lucasgazeau@lgazeau.fr>
2021-10-09 17:47:42 +0200

    before add store

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <Game></Game>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Game from '@/components/Game.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Game
+  }
+}
+</script>
+
+
+<style scoped>
+  .home{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+
+  }
+</style>
