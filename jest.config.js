e7f3a8f19c238679f741d4f310aa9dc7addaece9
Lyxas <lucasgazeau@lgazeau.fr>
2021-10-17 10:51:06 +0200

    better coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
   "collectCoverageFrom": [
-    "**/*.{js,jsx}",
+    "src/**/*.{js,jsx,vue}",
     "!**/node_modules/**",
+    "!src/plugins/*",
     "!**/vendor/**"
   ],
   "testMatch": ['**/tests/**/*.spec.{j,t}s?(x)']
